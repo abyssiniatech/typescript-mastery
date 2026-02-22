b57fb44f6af153b6b8ee46dc462b5b42e1c240ea
abyssiniatech <surafelmengist2015@gmail.com>
2026-02-22 20:52:07 +0300

    first commit

---
+++ dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const greet = (name) => {
+    return `Hello, ${name}! Welcome to TypeScript 🚀`;
+};
+console.log(greet("Surafel"));
+console.log("hello");
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+const greet = (name: string): string => {
+  return `Hello, ${name}! Welcome to TypeScript 🚀`;
+};
+
+console.log(greet("Surafel"));
+console.log("hello")
