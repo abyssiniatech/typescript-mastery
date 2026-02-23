274e3e91d35c970524a7ee23a7bbbc364f741ebd
abyssiniatech <surafelmengist2015@gmail.com>
2026-02-23 18:18:35 +0300

    add react setup

--- dist/index.js
+++ dist/index.js
@@ -1,16 +1,3 @@
 "use strict";
-// union types in typescript
 Object.defineProperty(exports, "__esModule", { value: true });
-const isAccess = (role) => {
-    if (role === "admin") {
-        console.log("you are admin");
-    }
-    else if (role === "user") {
-        console.log("you are user");
-    }
-    else {
-        console.log("you are guest");
-    }
-};
-isAccess("user");
 //# sourceMappingURL=index.js.map
